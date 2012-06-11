e3bc71d067e397bc4092ebf561aaab301fa9ee1a
IchHabRecht <IchHabRecht@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-11 09:24:20 +0000

    New Tag 1.6.8
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/powermail/tags/1.6.8@63336 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "powermail".
+#
+# Auto generated 07-06-2012 19:00
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'powermail',
+	'description' => 'Powerful and easy to use mailform extension with many features like storing form datas in database, export them to HTML, CSV and EXCEL (last one needs extension phpexcel_library), javascript validation (jQuery), multi step forms, different HTML templates, IRRE (Inline Relationship Record Editing), static_info_tables and many more...',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.6.8',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '4.4.6-0.0.0',
+	'PHP_version' => '5.2.0-0.0.0',
+	'module' => 'mod1',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => 'uploads/tx_powermail/files',
+	'modify_tables' => 'tt_content',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Powermail development team',
+	'author_email' => 'alexander.kellner@in2code.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
